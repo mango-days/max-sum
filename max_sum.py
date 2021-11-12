7eef473ace18de07bc708de3d105367e9deda150
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-13 04:50:06 +0530

    Update max_sum.py

--- max_sum.py
+++ max_sum.py
@@ -1,15 +1,15 @@
 array1 = [2,3,7,10,12]
 array2 = [1,5,7,8,9]
 
-def sort (array1, array2, ans):
+def max_sum (array1, array2, ans):
     index1=0
     index2=0
     while (index1<len(array1) and index2<len(array2)):
-        if array1[index1]==array2[index2]: return (sort (array2[index2+1:], array1[index1+1:], ans+array1[index1]))
+        if array1[index1]==array2[index2]: return (max_sum (array2[index2+1:], array1[index1+1:], ans+array1[index1]))
         else:   
             ans+=(array1[index1])
             index1+=1
             index2+=1
         if (index1==len(array1) or index2==len(array1)): return(ans)
         
-print (max (max(sort(array1, array2, 0), sort(array2, array1, 0)), max(sum(array1), sum(array2))) )
+print (max (max(max_sum(array1, array2, 0), max_sum(array2, array1, 0)), max(sum(array1), sum(array2))) )
