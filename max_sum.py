131e1046fdf26a9dabdcfcf98f832e2f6cb3133d
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-13 04:56:59 +0530

    Update max_sum.py

--- max_sum.py
+++ max_sum.py
@@ -7,9 +7,10 @@ def max_sum (array1, array2, ans):
     while (index1<len(array1) and index2<len(array2)):
         if array1[index1]==array2[index2]: return (max_sum (array2[index2+1:], array1[index1+1:], ans+array1[index1]))
         else:   
-            ans+=(array1[index1])
-            index1+=1
-            index2+=1
+            if array1[index1]<array2[index2]:
+                ans+=(array1[index1])
+                index1+=1
+            else : index2+=1
         if (index1==len(array1) or index2==len(array1)): return(ans)
         
 print (max (max(max_sum(array1, array2, 0), max_sum(array2, array1, 0)), max(sum(array1), sum(array2))) )
